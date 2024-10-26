5bd4dc465ebe6bc5c27ad32296715a3b86097de3
bike-92 <awh.daemen@gmail.com>
2024-10-26 20:23:14 +0200

    add new font and components

---
+++ src/routes/Game-component/Game.svelte
@@ -0,0 +1,9 @@
+<script>
+    const items = [1, 2, 3, 4];
+  </script>
+  
+  <div class="grid grid-cols-4 gap-4">
+    {#each items as item}
+      <div class="bg-blue-500 text-white p-4 rounded-md">{item}</div>
+    {/each}
+  </div>
