08f98f72d409558251300edf524a8cdfbb0f2a8a
bike-92 <awh.daemen@gmail.com>
2024-10-26 10:11:52 +0200

    create first components

---
+++ src/routes/Title-component/Title.svelte
@@ -0,0 +1,16 @@
+<div>
+    <h1 class="text-cyan-400">Mastermind</h1>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+        font-size: 50px;
+    }
+
+    h1 {
+        font-family: 'Syne Mono';
+    }
+</style>
